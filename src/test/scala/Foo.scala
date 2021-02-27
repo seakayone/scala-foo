2ff68c553051bc014891f461ef32cc9844035aa7
Christian Kleinboelting <kleinboelting@signal7.de>
2021-02-27 21:37:25 +0100

    some permutations

--- src/test/scala/Foo.scala
+++ src/test/scala/Foo.scala
@@ -1,22 +1,78 @@
-import collection.mutable.Stack
-import org.scalatest._
-import flatspec._
-import matchers._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
 
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+  val listA = List(1, 2)
+  val listB = List(3, 4)
+  val listC = List(5, 6)
+  val listD = List(7, 8)
+
+  def f(one: List[Int], two: List[Int]): List[List[Int]] = {
+    for (first <- one; second <- two) yield List(first, second)
+    //    one.flatMap(
+    //      first => two.map(second => List(first, second))
+    //    )
   }
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
+  def g(one: List[Int], two: List[Int], three: List[Int]): List[List[Int]] = {
+    //    for (first <- one; second <- two; third <- three) yield List(first, second, third)
+    one.flatMap(
+      first => two.map(second => List(first, second))
+    ).flatMap(
+      perm => three.map(third => perm :+ third)
+    )
+  }
+
+  def h(listOfLists: List[List[Int]]): List[List[Int]] = {
+    listOfLists match {
+      case Nil => List(List())
+      case one :: Nil => List(one)
+      case one :: two :: Nil => f(one, two)
+      case head :+ last=> hPerm(last, h(head))
     }
+  }
+
+  def hPerm(three: List[Int], two: List[List[Int]]): List[List[Int]] = {
+    two.flatMap(perm => three.map(l => perm :+ l))
+  }
+
+
+  "f" should "permutate" in {
+    f(listA, listB) should equal(
+      List(
+        List(1, 3), List(1, 4),
+        List(2, 3), List(2, 4)
+      ))
+  }
+
+  "g" should "permutate" in {
+    g(listA, listB, listC) should equal(
+      List(
+        List(1, 3, 5), List(1, 3, 6),
+        List(1, 4, 5), List(1, 4, 6),
+        List(2, 3, 5), List(2, 3, 6),
+        List(2, 4, 5), List(2, 4, 6)
+      ))
+  }
+
+  "hPerm" should "permutate" in {
+    hPerm(listC, f(listA, listB)) should equal(
+      List(
+        List(1, 3, 5), List(1, 3, 6),
+        List(1, 4, 5), List(1, 4, 6),
+        List(2, 3, 5), List(2, 3, 6),
+        List(2, 4, 5), List(2, 4, 6)
+      ))
+  }
+
+  "h" should "permutate" in {
+    h(List(listA, listB, listC)) should equal(
+      List(
+        List(1, 3, 5), List(1, 3, 6),
+        List(1, 4, 5), List(1, 4, 6),
+        List(2, 3, 5), List(2, 3, 6),
+        List(2, 4, 5), List(2, 4, 6)
+      ))
   }
 }
